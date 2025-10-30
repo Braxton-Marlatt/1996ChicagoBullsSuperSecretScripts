72391d4ced5e4b79e75a8627de355949e06a69a8
B3nton-Wilson <wils8062@vandals.uidaho.edu>
2025-10-30 14:33:53 -0700

    add: lenient rules for traffic out
    Updated iptables rules to enhance security by allowing established connections and essential services.

--- Linux/startup.sh
+++ Linux/startup.sh
@@ -124,16 +124,29 @@ function hardenIPtables {
     
     # Allow SSH
     iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-    iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
+    iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT    
     
     # Block everything by default
     iptables -t filter -P INPUT DROP
     iptables -t filter -P FORWARD DROP
     iptables -t filter -P OUTPUT DROP
+
+    # Allow inbound packets that are part of established connections
+    iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
     
     # Allow loopback communications
-    sudo iptables -A INPUT -i lo -j ACCEPT
-    sudo iptables -A OUTPUT -o lo -j ACCEPT
+    iptables -A INPUT -i lo -j ACCEPT
+    iptables -A OUTPUT -o lo -j ACCEPT
+
+    # Allow essential services
+    # DNS
+    iptables -A OUTPUT -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+    iptables -A OUTPUT -p tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+    
+    # HTTP / HTTPS
+    iptables -A OUTPUT -p tcp --dport 80  -m conntrack --ctstate NEW -j ACCEPT
+    iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT
+    
 }
 
 function setupSELinux {
